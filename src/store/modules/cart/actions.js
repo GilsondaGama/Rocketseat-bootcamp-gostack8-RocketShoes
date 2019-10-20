5f48fc05ec75e214758b15ae4e5e41373c550a10
Gilson da Gama <gilsondagama@hotmail.com>
2019-10-20 10:08:36 -0300

    Refatorando as actions

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,36 +1,13 @@
-export function addToCartRequest(id) {
+export function addToCart(product) {
   return {
-    type: '@cart/ADD_REQUEST',
-    id,
-  }
-}
-
-export function addToCartSuccess(product) {
-  return {
-    type: '@cart/ADD_SUCCESS',
+    type: '@cart/ADD',
     product,
-  }
+  };
 }
 
 export function removeFromCart(id) {
   return {
     type: '@cart/REMOVE',
     id,
-  }
-}
-
-export function updateAmountRequest(id, amount) {
-  return {
-    type: '@cart/UPDATE_AMOUNT_REQUEST',
-    id,
-    amount,
-  }
-}
-
-export function updateAmountSuccess(id, amount) {
-  return {
-    type: '@cart/UPDATE_AMOUNT_SUCCESS',
-    id,
-    amount,
-  }
+  };
 }
